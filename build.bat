b0fa3d0ac7785ee920dd9424c4afbd50bb51e0db
zlumer <zlumer@gmail.com>
2018-05-24 13:06:27 +0300

    initial commit

---
+++ build.bat
@@ -0,0 +1,9 @@
+dotnet publish -o ../ico-avm
+@rem cd ../neo-compiler
+@rem dotnet publish -o ../ico-avm
+if %errorlevel% neq 0 exit /b %errorlevel%
+pushd ..\ico-avm
+dotnet neon.dll NeoContractIco.dll
+if %errorlevel% neq 0 exit /b %errorlevel%
+popd
+xcopy ..\ico-avm\NeoContractIco.avm .\output\
